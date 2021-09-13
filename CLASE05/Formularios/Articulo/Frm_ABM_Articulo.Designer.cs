3f5e736020c9bbc7afdd707d0514651116abb945
Agustin Espeche <leyvidson@gmail.com>
2021-09-13 20:01:49 -0300

    abm Lote+Articulo

---
+++ CLASE05/Formularios/Articulo/Frm_ABM_Articulo.Designer.cs
@@ -0,0 +1,307 @@
+﻿
+namespace CLASE05.Formularios.Articulo
+{
+    partial class Frm_ABM_Articulo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_patron = new System.Windows.Forms.MaskedTextBox();
+            this.rb_id_usuario = new System.Windows.Forms.RadioButton();
+            this.rb_n_usuario = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton7);
+            this.groupBox1.Controls.Add(this.radioButton8);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_patron);
+            this.groupBox1.Controls.Add(this.rb_id_usuario);
+            this.groupBox1.Controls.Add(this.rb_n_usuario);
+            this.groupBox1.Size = new System.Drawing.Size(645, 251);
+            this.groupBox1.Controls.SetChildIndex(this.btn_buscar, 0);
+            this.groupBox1.Controls.SetChildIndex(this.rb_n_usuario, 0);
+            this.groupBox1.Controls.SetChildIndex(this.rb_id_usuario, 0);
+            this.groupBox1.Controls.SetChildIndex(this.txt_patron, 0);
+            this.groupBox1.Controls.SetChildIndex(this.label2, 0);
+            this.groupBox1.Controls.SetChildIndex(this.radioButton1, 0);
+            this.groupBox1.Controls.SetChildIndex(this.radioButton2, 0);
+            this.groupBox1.Controls.SetChildIndex(this.radioButton6, 0);
+            this.groupBox1.Controls.SetChildIndex(this.radioButton5, 0);
+            this.groupBox1.Controls.SetChildIndex(this.radioButton4, 0);
+            this.groupBox1.Controls.SetChildIndex(this.radioButton3, 0);
+            this.groupBox1.Controls.SetChildIndex(this.radioButton8, 0);
+            this.groupBox1.Controls.SetChildIndex(this.radioButton7, 0);
+            // 
+            // btn_buscar
+            // 
+            this.btn_buscar.Location = new System.Drawing.Point(536, 202);
+            // 
+            // btn_alta
+            // 
+            this.btn_alta.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_alta.FlatAppearance.BorderSize = 0;
+            this.btn_alta.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_alta.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_alta.Location = new System.Drawing.Point(489, 359);
+            // 
+            // btn_modificar
+            // 
+            this.btn_modificar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_modificar.FlatAppearance.BorderSize = 0;
+            this.btn_modificar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_modificar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_modificar.Location = new System.Drawing.Point(550, 359);
+            // 
+            // btn_borrar
+            // 
+            this.btn_borrar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_borrar.FlatAppearance.BorderSize = 0;
+            this.btn_borrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_borrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_borrar.Location = new System.Drawing.Point(607, 356);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Size = new System.Drawing.Size(248, 31);
+            this.label1.Text = "Consulta de Articulo";
+            // 
+            // btn_consultar
+            // 
+            this.btn_consultar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_consultar.FlatAppearance.BorderSize = 0;
+            this.btn_consultar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_consultar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_consultar.Location = new System.Drawing.Point(427, 358);
+            // 
+            // btn_cerrar
+            // 
+            this.btn_cerrar.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btn_cerrar.Location = new System.Drawing.Point(637, 1);
+            // 
+            // btn_minimizar
+            // 
+            this.btn_minimizar.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btn_minimizar.Location = new System.Drawing.Point(567, 1);
+            // 
+            // btn_maximizar
+            // 
+            this.btn_maximizar.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btn_maximizar.Location = new System.Drawing.Point(602, 1);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.radioButton2.Location = new System.Drawing.Point(331, 181);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(115, 21);
+            this.radioButton2.TabIndex = 29;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "id_pais_origen";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.radioButton1.Location = new System.Drawing.Point(331, 154);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(78, 21);
+            this.radioButton1.TabIndex = 28;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "id_rubro";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 17);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Patrón de Búsqueda";
+            // 
+            // txt_patron
+            // 
+            this.txt_patron.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.txt_patron.Enabled = false;
+            this.txt_patron.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_patron.ForeColor = System.Drawing.Color.Gainsboro;
+            this.txt_patron.Location = new System.Drawing.Point(154, 50);
+            this.txt_patron.Mask = "LLLLLLLLLLLLLLLLLLLL";
+            this.txt_patron.Name = "txt_patron";
+            this.txt_patron.Size = new System.Drawing.Size(129, 25);
+            this.txt_patron.TabIndex = 26;
+            // 
+            // rb_id_usuario
+            // 
+            this.rb_id_usuario.AutoSize = true;
+            this.rb_id_usuario.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.rb_id_usuario.Location = new System.Drawing.Point(331, 127);
+            this.rb_id_usuario.Name = "rb_id_usuario";
+            this.rb_id_usuario.Size = new System.Drawing.Size(74, 21);
+            this.rb_id_usuario.TabIndex = 25;
+            this.rb_id_usuario.TabStop = true;
+            this.rb_id_usuario.Text = "nombre";
+            this.rb_id_usuario.UseVisualStyleBackColor = true;
+            // 
+            // rb_n_usuario
+            // 
+            this.rb_n_usuario.AutoSize = true;
+            this.rb_n_usuario.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.rb_n_usuario.Location = new System.Drawing.Point(331, 100);
+            this.rb_n_usuario.Name = "rb_n_usuario";
+            this.rb_n_usuario.Size = new System.Drawing.Size(96, 21);
+            this.rb_n_usuario.TabIndex = 24;
+            this.rb_n_usuario.TabStop = true;
+            this.rb_n_usuario.Text = "plazo_pago";
+            this.rb_n_usuario.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.radioButton3.Location = new System.Drawing.Point(154, 181);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(60, 21);
+            this.radioButton3.TabIndex = 33;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "costo";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.radioButton4.Location = new System.Drawing.Point(154, 154);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(85, 21);
+            this.radioButton4.TabIndex = 32;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "numSerie";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.radioButton5.Location = new System.Drawing.Point(154, 127);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(81, 21);
+            this.radioButton5.TabIndex = 31;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "numLote";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.radioButton6.Location = new System.Drawing.Point(154, 100);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(69, 21);
+            this.radioButton6.TabIndex = 30;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "codigo";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.radioButton7.Location = new System.Drawing.Point(154, 208);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(110, 21);
+            this.radioButton7.TabIndex = 35;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "tiempo_envio";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.radioButton8.Location = new System.Drawing.Point(331, 208);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(107, 21);
+            this.radioButton8.TabIndex = 34;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "id_proveedor";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // Frm_ABM_Articulo
+            // 
+            this._PosiciónVisible = true;
+            this._titulo = "Consulta de Articulo";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 411);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "Frm_ABM_Articulo";
+            this.Text = "Frm_ABM_Articulo";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox txt_patron;
+        private System.Windows.Forms.RadioButton rb_id_usuario;
+        private System.Windows.Forms.RadioButton rb_n_usuario;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+    }
+}
---
+++ CLASE05/Formularios/Articulo/Frm_ABM_Articulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CLASE05.Formularios.Articulo
+{
+    public partial class Frm_ABM_Articulo : CLASE05.Formularios.FrmBASE.FrmBase_ABM_Entrada
+    {
+        public Frm_ABM_Articulo()
+        {
+            InitializeComponent();
+        }
+    }
+}

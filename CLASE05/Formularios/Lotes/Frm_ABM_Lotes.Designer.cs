e58227bfaa0c5a72fa0271b2691148935bca91c4
Agustin Espeche <leyvidson@gmail.com>
2021-09-13 19:27:08 -0300

    ABM_LOTE

---
+++ CLASE05/Formularios/Lotes/Frm_ABM_Lotes.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace CLASE05.Formularios.Lotes
+{
+    partial class Frm_ABM_Lotes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.rb_n_usuario = new System.Windows.Forms.RadioButton();
+            this.rb_id_usuario = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_patron = new System.Windows.Forms.MaskedTextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_patron);
+            this.groupBox1.Controls.Add(this.rb_id_usuario);
+            this.groupBox1.Controls.Add(this.rb_n_usuario);
+            this.groupBox1.Size = new System.Drawing.Size(645, 130);
+            this.groupBox1.Controls.SetChildIndex(this.rb_n_usuario, 0);
+            this.groupBox1.Controls.SetChildIndex(this.rb_id_usuario, 0);
+            this.groupBox1.Controls.SetChildIndex(this.txt_patron, 0);
+            this.groupBox1.Controls.SetChildIndex(this.label2, 0);
+            this.groupBox1.Controls.SetChildIndex(this.btn_buscar, 0);
+            this.groupBox1.Controls.SetChildIndex(this.radioButton1, 0);
+            this.groupBox1.Controls.SetChildIndex(this.radioButton2, 0);
+            // 
+            // btn_buscar
+            // 
+            this.btn_buscar.Location = new System.Drawing.Point(554, 90);
+            // 
+            // btn_alta
+            // 
+            this.btn_alta.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_alta.FlatAppearance.BorderSize = 0;
+            this.btn_alta.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_alta.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_alta.Location = new System.Drawing.Point(505, 404);
+            // 
+            // btn_modificar
+            // 
+            this.btn_modificar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_modificar.FlatAppearance.BorderSize = 0;
+            this.btn_modificar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_modificar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_modificar.Location = new System.Drawing.Point(556, 404);
+            // 
+            // btn_borrar
+            // 
+            this.btn_borrar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_borrar.FlatAppearance.BorderSize = 0;
+            this.btn_borrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_borrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_borrar.Location = new System.Drawing.Point(607, 404);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(15, 25);
+            this.label1.Size = new System.Drawing.Size(208, 31);
+            this.label1.Text = "Consulta de Lote";
+            // 
+            // btn_consultar
+            // 
+            this.btn_consultar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_consultar.FlatAppearance.BorderSize = 0;
+            this.btn_consultar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_consultar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btn_consultar.Location = new System.Drawing.Point(452, 402);
+            // 
+            // btn_cerrar
+            // 
+            this.btn_cerrar.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btn_cerrar.Location = new System.Drawing.Point(649, 1);
+            // 
+            // btn_minimizar
+            // 
+            this.btn_minimizar.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btn_minimizar.Location = new System.Drawing.Point(579, 1);
+            // 
+            // btn_maximizar
+            // 
+            this.btn_maximizar.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btn_maximizar.Location = new System.Drawing.Point(614, 1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(10, 217);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(647, 181);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // rb_n_usuario
+            // 
+            this.rb_n_usuario.AutoSize = true;
+            this.rb_n_usuario.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.rb_n_usuario.Location = new System.Drawing.Point(301, 18);
+            this.rb_n_usuario.Name = "rb_n_usuario";
+            this.rb_n_usuario.Size = new System.Drawing.Size(84, 21);
+            this.rb_n_usuario.TabIndex = 13;
+            this.rb_n_usuario.TabStop = true;
+            this.rb_n_usuario.Text = "num_lote";
+            this.rb_n_usuario.UseVisualStyleBackColor = true;
+            // 
+            // rb_id_usuario
+            // 
+            this.rb_id_usuario.AutoSize = true;
+            this.rb_id_usuario.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.rb_id_usuario.Location = new System.Drawing.Point(301, 45);
+            this.rb_id_usuario.Name = "rb_id_usuario";
+            this.rb_id_usuario.Size = new System.Drawing.Size(163, 21);
+            this.rb_id_usuario.TabIndex = 18;
+            this.rb_id_usuario.TabStop = true;
+            this.rb_id_usuario.Text = "nro_remito_proveedor";
+            this.rb_id_usuario.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 17);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Patrón de Búsqueda";
+            // 
+            // txt_patron
+            // 
+            this.txt_patron.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.txt_patron.Enabled = false;
+            this.txt_patron.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_patron.ForeColor = System.Drawing.Color.Gainsboro;
+            this.txt_patron.Location = new System.Drawing.Point(142, 49);
+            this.txt_patron.Mask = "LLLLLLLLLLLLLLLLLLLL";
+            this.txt_patron.Name = "txt_patron";
+            this.txt_patron.Size = new System.Drawing.Size(129, 25);
+            this.txt_patron.TabIndex = 20;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.radioButton1.Location = new System.Drawing.Point(301, 72);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(74, 21);
+            this.radioButton1.TabIndex = 22;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "nombre";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.radioButton2.Location = new System.Drawing.Point(301, 99);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(113, 21);
+            this.radioButton2.TabIndex = 23;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "fecha_compra";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Frm_ABM_Lotes
+            // 
+            this._PosiciónVisible = true;
+            this._titulo = "Consulta de Lote";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 453);
+            this.Controls.Add(this.dataGridView1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "Frm_ABM_Lotes";
+            this.Text = "Frm_ABM_Lotes";
+            this.Controls.SetChildIndex(this.dataGridView1, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.groupBox1, 0);
+            this.Controls.SetChildIndex(this.btn_alta, 0);
+            this.Controls.SetChildIndex(this.btn_modificar, 0);
+            this.Controls.SetChildIndex(this.btn_borrar, 0);
+            this.Controls.SetChildIndex(this.btn_consultar, 0);
+            this.Controls.SetChildIndex(this.lbl_posicion, 0);
+            this.Controls.SetChildIndex(this.btn_cerrar, 0);
+            this.Controls.SetChildIndex(this.btn_maximizar, 0);
+            this.Controls.SetChildIndex(this.btn_minimizar, 0);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RadioButton rb_n_usuario;
+        private System.Windows.Forms.RadioButton rb_id_usuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox txt_patron;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
---
+++ CLASE05/Formularios/Lotes/Frm_Alta_Lote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CLASE05.Formularios.Lotes
+{
+    public partial class Frm_Alta_Lote : CLASE05.Formularios.FrmBASE.FrmBase
+    {
+        public Frm_Alta_Lote()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_titulo_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
